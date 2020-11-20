0456727c2decaf4b2133a368278c2af963e94d6b
Milan Felix Šulc <rkfelix@gmail.com>
2020-11-20 21:40:18 +0100

    Slim: simplify

--- php-slim/api/index.php
+++ php-slim/api/index.php
@@ -1,13 +1,12 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-// Now PHP
+require __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$app = new \Slim\App;
+$app = AppFactory::create();
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
